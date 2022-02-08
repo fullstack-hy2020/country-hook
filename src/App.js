7035a6b3aab6e28dbe14a56662999c0a8a55f3d1
Matti Luukkainen <mluukkai@iki.fi>
2022-02-08 02:28:46 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -25,25 +25,17 @@ const useCountry = (name) => {
 
 const Country = ({ country }) => {
   if (!country) {
-    return null
-  }
-
-  if (!country.found) {
-    return (
-      <div>
-        not found...
-      </div>
-    )
+    return <div>not found...</div>
   }
 
   return (
     <div>
-      <h3>{country.data.name} </h3>
-      <div>capital {country.data.capital} </div>
-      <div>population {country.data.population}</div> 
-      <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`}/>  
+      <h3>{country.name.common}</h3>
+      <div>population {country.population}</div> 
+      <div>capital {country.capital}</div>
+      <img src={country.flags.png} height='100' alt={`flag of ${country.name.common}`}/> 
     </div>
-  )
+  )  
 }
 
 const App = () => {
