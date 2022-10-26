6fe6f24a3a501fd2c8718d0c59ca17d7be4035d7
Ivan Hu <47449822+JBlakd@users.noreply.github.com>
2022-10-26 12:12:54 +1100

    Rearranged capital and population in Country component
    So that it corresponds to the screenshot in 7.7 https://fullstackopen.com/en/part7/custom_hooks

--- src/App.js
+++ src/App.js
@@ -31,8 +31,8 @@ const Country = ({ country }) => {
   return (
     <div>
       <h3>{country.name.common}</h3>
-      <div>population {country.population}</div> 
       <div>capital {country.capital}</div>
+      <div>population {country.population}</div> 
       <img src={country.flags.png} height='100' alt={`flag of ${country.name.common}`}/> 
     </div>
   )  
